0695194438f427b63be8f17764120e07d41da89c
katebolanos <32135090+katebolanos@users.noreply.github.com>
2024-05-31 16:56:32 +0800

    Header Edit

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -3,15 +3,17 @@
         <div class="row">
             <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
                 <div class="logo-area">
-                    <a href="#"><h1 class="text-logo">FINGAL MEMORIALS LTD</h1></a>
+                    <a href="#">
+                        <h1 class="text-logo">FINGAL MEMORIALS LTD</h1>
+                    </a>
                 </div>
             </div>
             <div class="col-lg-8 col-md-8 col-sm-12 col-xs-12">
                 <div class="header-top-menu">
                     <ul class="nav navbar-nav notika-top-nav">
-                        <li>{{Auth::user()->firstname}} {{Auth::user()->lastname}}</li>
-                        <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><span><i class="fa fa-user"></i></span></a>
-                            <div role="menu" class="dropdown-menu message-dd chat-dd animated zoomIn">
+                        <li class="nav-item"><a href="#" data-toggle="dropdown" role="button"
+                                aria-expanded="false" class="nav-link dropdown-toggle"><span><img src="{{ asset('images/user.png') }}" style="width:40px;height:40px;margin-right: 5px;"></span> {{ Auth::user()->firstname }} {{ Auth::user()->lastname }}</a>
+                            <div role="menu" class="dropdown-menu message-dd chat-dd animated fadeIn">
                                 <div class="hd-message-info">
                                     <a href="#">
                                         <div class="hd-message-sn">
@@ -58,7 +60,7 @@
                                     <li><a href="/complete-unsettled">Complete/Invoiced Unsettled</a></li>
                                 </ul>
                             </li>
-                            <li ><a data-toggle="collapse" data-target="#schedules-mob" href="#">Schedules</a>
+                            <li><a data-toggle="collapse" data-target="#schedules-mob" href="#">Schedules</a>
                                 <ul id="schedules-mob" class="collapse dropdown-header-top">
                                     <li><a href="/new-memorial">New Memorial</a></li>
                                     <li><a href="/renovation">Renovation</a></li>
@@ -76,9 +78,16 @@
                             </li>
                             <li><a data-toggle="collapse" data-target="#admin-mob" href="#">Admin</a>
                                 <ul id="admin-mob" class="collapse dropdown-header-top">
-                                    <li><a href="/users">Users</a></li>
+                                    <li><a href="/account-types">Account Types</a></li>
                                     <li><a href="/branches">Branches</a></li>
                                     <li><a href="/document-types">Document Types</a></li>
+                                    <li><a href="/order-types">Order Types</a></li>
+                                    <li><a href="/payment-types">Payment Types</a></li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#admin-mob" href="#">User</a>
+                                <ul id="admin-mob" class="collapse dropdown-header-top">
+                                    <li><a href="/users">Users</a></li>
                                 </ul>
                             </li>
                         </ul>
@@ -95,20 +104,32 @@
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                 <ul class="nav nav-tabs notika-menu-wrap menu-it-icon-pro">
-                    <li class="{{ Request::is('order','order/*','customer','customer/*','complete-unsettled','complete-unsettled/*',) ? 'active' : '' }}"><a data-toggle="tab" href="#orders"><i class="fa fa-list"></i> Orders</a></li>
-                    <li class="{{ Request::is('new-memorial','new-memorial/*','renovation','renovation/*','sundries','sundries/*','cremation-product','cremation-product/*','other','other/*',) ? 'active' : '' }}"><a data-toggle="tab" href="#schedules"><i class="fa fa-calendar"></i> Schedules</a></li>
-                    <li class="{{ Request::is('order-summary','order-summary/*','order-details','order-details/*','order-report','order-report/*',) ? 'active' : '' }}"><a data-toggle="tab" href="#reports"><i class="fa fa-folder-open-o"></i> Reports</a></li>
-                    <li class=" {{ Request::is('users','users/*','branches','branches/*','document-types','document-types/*',) ? 'active' : '' }}" ><a data-toggle="tab" href="#admin"><i class="fa fa-cog"></i> Admin</a></li>
+                    <li
+                        class="{{ Request::is('order', 'order/*', 'customer', 'customer/*', 'complete-unsettled', 'complete-unsettled/*') ? 'active' : '' }}">
+                        <a data-toggle="tab" href="#orders"><i class="fa fa-list"></i> Orders</a></li>
+                    <li
+                        class="{{ Request::is('new-memorial', 'new-memorial/*', 'renovation', 'renovation/*', 'sundries', 'sundries/*', 'cremation-product', 'cremation-product/*', 'other', 'other/*') ? 'active' : '' }}">
+                        <a data-toggle="tab" href="#schedules"><i class="fa fa-calendar"></i> Schedules</a></li>
+                    <li
+                        class="{{ Request::is('order-summary', 'order-summary/*', 'order-details', 'order-details/*', 'order-report', 'order-report/*') ? 'active' : '' }}">
+                        <a data-toggle="tab" href="#reports"><i class="fa fa-folder-open-o"></i> Reports</a></li>
+                    <li
+                        class=" {{ Request::is('branches', 'branches/*', 'document-types', 'document-types/*', 'cemetery', 'cemetery/*', 'cemetery-group', 'cemetery-group/*', 'cemetery-area', 'cemetery-area/*') ? 'active' : '' }}">
+                        <a data-toggle="tab" href="#admin"><i class="fa fa-cog"></i> Admin Utilities</a></li>
+                    <li class=" {{ Request::is('users', 'users/*') ? 'active' : '' }}"><a data-toggle="tab"
+                            href="#user"><i class="fa fa-user"></i> User</a></li>
                 </ul>
                 <div class="tab-content custom-menu-content">
-                    <div id="orders" class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('order','order/*','customer','customer/*','complete-unsettled','complete-unsettled/*',) ? 'active' : '' }}">
+                    <div id="orders"
+                        class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('order', 'order/*', 'customer', 'customer/*', 'complete-unsettled', 'complete-unsettled/*') ? 'active' : '' }}">
                         <ul class="notika-main-menu-dropdown">
                             <li><a href="/order">Orders</a></li>
                             <li><a href="/customer">Customer</a></li>
                             <li><a href="/complete-unsettled">Complete/Invoiced Unsettled</a></li>
                         </ul>
                     </div>
-                    <div id="schedules" class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('new-memorial','new-memorial/*','renovation','renovation/*','sundries','sundries/*','cremation-product','cremation-product/*','other','other/*',) ? 'active' : '' }}">
+                    <div id="schedules"
+                        class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('new-memorial', 'new-memorial/*', 'renovation', 'renovation/*', 'sundries', 'sundries/*', 'cremation-product', 'cremation-product/*', 'other', 'other/*') ? 'active' : '' }}">
                         <ul class="notika-main-menu-dropdown">
                             <li><a href="/new-memorial">New Memorial</a></li>
                             <li><a href="/renovation">Renovation</a></li>
@@ -117,22 +138,32 @@
                             <li><a href="/other">Other</a></li>
                         </ul>
                     </div>
-                    <div id="reports" class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('order-summary','order-summary/*','order-details','order-details/*','order-report','order-report/*',) ? 'active' : '' }}">
+                    <div id="reports"
+                        class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('order-summary', 'order-summary/*', 'order-details', 'order-details/*', 'order-report', 'order-report/*') ? 'active' : '' }}">
                         <ul class="notika-main-menu-dropdown">
                             <li><a href="/order-summary">Order Summary</a></li>
                             <li><a href="/order-details">Order Details</a></li>
                             <li><a href="/order-report">Order Report</a></li>
                         </ul>
                     </div>
-                    <div id="admin" class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('users','users/*','branches','branches/*','document-types','document-types/*','branches','branches/*','account-types','account-types/*','categories','categories/*','order-types','order-types/*','payment-types','payment-types/*',) ? 'active' : '' }}">
+                    <div id="admin"
+                        class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('branches', 'branches/*', 'document-types', 'document-types/*', 'branches', 'branches/*', 'account-types', 'account-types/*', 'categories', 'categories/*', 'order-types', 'order-types/*', 'payment-types', 'payment-types/*', 'cemetery', 'cemetery/*', 'cemetery-group', 'cemetery-group/*', 'cemetery-area', 'cemetery-area/*') ? 'active' : '' }}">
                         <ul class="notika-main-menu-dropdown">
-                            <li><a href="/users">Users</a></li>
-                            <li><a href="/branches">Branches</a></li>
-                            <li><a href="/document-types">Document Types</a></li>
                             <li><a href="/account-types">Account Types</a></li>
+                            <li><a href="/branches">Branches</a></li>
                             <li><a href="/categories">Categories</a></li>
+                            <li><a href="/cemetery">Cemetery</a></li>
+                            <li><a href="/cemetery-area">Cemetery Area</a></li>
+                            <li><a href="/cemetery-group">Cemetery Group</a></li>
+                            <li><a href="/document-types">Document Types</a></li>
                             <li><a href="/order-types">Order Types</a></li>
                             <li><a href="/payment-types">Payment Types</a></li>
+                        </ul>
+                    </div>
+                    <div id="user"
+                        class="tab-pane notika-tab-menu-bg animated flipInX {{ Request::is('users', 'users/*') ? 'active' : '' }}">
+                        <ul class="notika-main-menu-dropdown">
+                            <li><a href="/users">Users</a></li>
                         </ul>
                     </div>
                 </div>
