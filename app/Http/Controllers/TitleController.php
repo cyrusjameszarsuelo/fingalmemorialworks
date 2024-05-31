f900f5cf2b406650cb0263b53b2a4a7f9070406f
katebolanos <32135090+katebolanos@users.noreply.github.com>
2024-06-01 00:02:54 +0800

    Title Module

--- app/Http/Controllers/TitleController.php
+++ app/Http/Controllers/TitleController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Title;
 use Illuminate\Http\Request;
+use Auth;
+use Response;
 
 class TitleController extends Controller
 {
@@ -12,7 +14,9 @@ class TitleController extends Controller
      */
     public function index()
     {
-        //
+        $titles = Title::all();
+        return view('pages.title.index')
+            ->withTitles($titles);
     }
 
     /**
@@ -20,7 +24,7 @@ class TitleController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.title.form');
     }
 
     /**
@@ -28,7 +32,13 @@ class TitleController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate(self::formRule());
+        $request->merge([
+            'created_by' => Auth::id()
+        ]);
+        Title::create($request->all());
+        return redirect('/title')
+            ->with('success','Created Successfully!');
     }
 
     /**
@@ -42,24 +52,43 @@ class TitleController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Title $title)
+    public function edit(Request $request, String $id)
     {
-        //
+        $title = Title::find($id);
+        return view('pages.title.form')
+        ->withTitle($title);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Title $title)
+    public function update(Request $request, String $id)
     {
-        //
+        $request->validate(self::formRule($id));
+        $request->merge([
+            'updated_by' => Auth::id()
+        ]);
+        $source = Title::find($id);
+        $source->update($request->all());
+        return redirect('/title')
+            ->with('success','Updated Successfully!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Title $title)
+    public function destroy(Request $request)
     {
-        //
+        $id                             = $request->id;
+        $title = $titleData = Title::findOrFail($id);
+        $title->update(['deleted_by' => Auth::id()]);
+        $title->delete();
+        return Response::json($titleData);
+    }
+
+    public function formRule($id = false){
+        return [
+            "name"      => ['required','string']
+        ];
     }
 }
